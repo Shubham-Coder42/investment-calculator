8d26d1a36bb5b9add46bd6341be2d87b5a75baa0
Shubham-design-rgb <75521215+Shubham-design-rgb@users.noreply.github.com>
2025-08-16 00:04:51 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/investment-calculator',
   server:{
     port: 3000,
     host: true // Allow access from mobile devices on same network
